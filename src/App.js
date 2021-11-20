bf84522087378dc4ef86e3aaa4e6f758b8f899e2
Eduard <e.chernobrovkin@inspiritum.com>
2021-11-20 21:18:13 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [note, setNote] = useState([{id: null, text: ''}]);
+  const [noteText, setnoteText] = useState('');
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    if(note.length > 1){
+      sendData(note.length)
+    }
+    loadNotes()
+    
+  }, [note]);
+
+  const changeHandler = (e) => {
+    setnoteText(e.target.value)
+  }
+
+  const submitHadler = async (e) => {
+    e.preventDefault();
+    setNote(prev => [...prev, {id: note.length + 1, text: noteText}])
+    setnoteText('')
+    setTimeout(()=>{
+      loadNotes()
+    },150)
+  }
+
+  const removeHandler = (e) => {
+    let parent = e.target.closest('div'),
+        parentId = parent.id
+    fetch(`http://localhost:7777/notes/${parentId}`,{method: 'DELETE'})
+    setTimeout(()=>{
+      loadNotes()
+    },150)
+  }
+
+  const reload = () => {
+    loadNotes()
+  }
+
+  const loadNotes = () => {
+    fetch("http://localhost:7777/notes")
+    .then(response=>response.json())
+    .then(notes=>{
+      setNotes(notes)
+    })
+  }
+
+  const sendData = async (id) => {
+    let body = note.filter(note => note.id === id)
+    await fetch("http://localhost:7777/notes",{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(body[0])
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <textarea value={noteText} onChange={changeHandler}/>
+        <button type="submit" onClick={submitHadler}>add</button>
+      </form>
+      <button onClick={reload}>Обновить</button>
+      <div className="cardwrapper">
+        {notes.map(item => { return (<div className="card" id={item.id} key={item.id}>{item.text}<span onClick={removeHandler}>  X</span></div>)})}
+      </div>
     </div>
   );
 }
